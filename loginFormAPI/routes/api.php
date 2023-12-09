109d31df5212ea41d0c357b85714ac9f4c9e8957
emmanjames12 <dualloemmanuellejames@gmail.com>
2023-12-09 16:59:45 +0800

    Oct 9 5:00 PM

---
+++ loginFormAPI/routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\API\RegisterController;
+use App\Http\Controllers\API\ProductController;
+
+Route::post('register', 'RegisterController@register');
+Route::post('login', [RegisterController::class, 'login']);
+
+Route::middleware('auth:api')->group(function () {
+    Route::resource('products', ProductController::class);
+});
